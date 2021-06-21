8bdf8fc5ebeb1a523081bfadd3348426533559a7
Brady Roscher <bradyroscher@yahoo.com>
2021-06-21 16:10:27 -0400

    add forms and state updates for sign up page and log in page

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginPage = (props) => {
-  return <div></div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+  }
+  console.log(email)
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+  console.log(password)
+
+  return (
+    <div>
+      <form>
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
