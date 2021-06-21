9b029d95f0af959d9e432d3d3ca17f3d451a2257
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-06-21 13:28:53 -0700

    User Migration updated Capitlized turned into LowerCase  Brought USERJS into Migration folder Updated the created and update

--- migrations/20210621171221-create-user.js
+++ migrations/20210621171221-create-user.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,37 +9,49 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: DataTypes.VARCHAR(255),
+        allowNull: false
       },
       username: {
-        type: Sequelize.STRING
+        type: DataTypes.VARCHAR(255),
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: DataTypes.VARCHAR(255),
+        allowNull: false,
+        validate: { isEmail: true }
       },
       password: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       },
       location: {
-        type: Sequelize.INTEGER
+        type: DataTypes.INTEGER,
+        allowNull: false
       },
       bio: {
-        type: Sequelize.STRING
+        type: DataTypes.VARCHAR(255),
+        allowNull: true
       },
       image: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: true
       },
       createdAt: {
+        field: 'created_at',
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       },
       updatedAt: {
+        field: 'updated_at',
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: new Date()
       }
-    });
+    })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users')
   }
-};
+}
