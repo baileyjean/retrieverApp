04a01338f5264baf552c9a7a6120eb5b7155844a
Lindsey Moss <llmodbls@gmail.com>
2021-06-22 17:59:57 -0400

    added placeholder image for BrowsePage, additional mobile optimization styling for BrowsePage

--- client/src/pages/BrowsePage.js
+++ client/src/pages/BrowsePage.js
@@ -4,19 +4,20 @@ import amphibians from '../styles/images/amphibians.png'
 import birds from '../styles/images/birds.png'
 import felines from '../styles/images/felines.png'
 import canines from '../styles/images/canines.png'
+import tbd from '../styles/images/tbd.png'
 
 const BrowsePage = (props) => {
   const species = [
     { name: 'Reptile', img: `${reptiles}` },
     { name: 'Amphibian', img: `${amphibians}` },
     { name: 'Bird', img: `${birds}` },
-    { name: 'Insect', img: `${reptiles}` },
-    { name: 'Arachnid', img: `${reptiles}` },
-    { name: 'Rodent', img: `${reptiles}` },
-    { name: 'Weasel', img: `${reptiles}` },
+    { name: 'Insect', img: `${tbd}` },
+    { name: 'Arachnid', img: `${tbd}` },
+    { name: 'Rodent', img: `${tbd}` },
+    { name: 'Weasel', img: `${tbd}` },
     { name: 'Dog', img: `${canines}` },
     { name: 'Cat', img: `${felines}` },
-    { name: 'Fish', img: `${reptiles}` }
+    { name: 'Fish', img: `${tbd}` }
   ]
   return (
       <div className="browse-results">
