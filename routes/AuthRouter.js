c572a16e75bb59fc2fb77748df5a10d665400b6b
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-23 07:34:29 -0700

    Clean up CommentRouter and CommentController

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -3,6 +3,5 @@ const controller = require('../controllers/AuthController')
 
 Router.post('/login', controller.Login)
 Router.post('/register', controller.Register)
-// Router.delete('/deletepet', controller.DeleteMyPet)
 
 module.exports = Router;
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,15 +2,11 @@ const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
 Router.post('/', controller.AddComment)
-
 Router.get('/', controller.GetAllComments)
 Router.get('/user/:user_id', controller.GetCommentByUserId)
 Router.get('/pet/:pet_id', controller.GetCommentByPetId)
-
-Router.delete('/:user_id', controller.DeleteComment)
-
 Router.put('/:user_id', controller.UpdateComment)
+Router.delete('/:user_id', controller.DeleteComment)
 
 module.exports = Router
 
-//GetCommentByPetId
