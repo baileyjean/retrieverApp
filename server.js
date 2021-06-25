7c07ddf388e429ee2f80f67909fbca444a8c3f7b
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-25 13:37:40 -0700

    Prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+const path = require('path')
 const app = express()
 
 const AppRouter = require('./routes/AppRouter')
@@ -13,5 +14,12 @@ app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.json({ message: 'Server Works' }))
 app.use('/api', AppRouter)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
