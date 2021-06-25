2f0ed10f44ffca0b388b741b2ba20962c2f06cad
Alexander Zerra <Alexander.Zerra@gmail.com>
2021-06-24 19:29:24 -0700

    appjs authentication not working on refresh

--- client/src/App.js
+++ client/src/App.js
@@ -18,13 +18,14 @@ import { useHistory } from 'react-router-dom'
 // import { BASE_URL } from './globals'
 
 function App() {
-  const [opened, setOpen] = useState(false);
+  const [opened, setOpen] = useState(false)
   const [loggedIn, setLogIn] = useState(false)
   const [userID, setUserID] = useState('')
   const [userLocation, setUserLocation] = useState('')
   // const [specie, setSpecie] = useState([])
   const history = useHistory()
-  const [keyword,setKeyword] = useState('')
+  const [keyword, setKeyword] = useState('')
+  const [authenticated, setAuthenticated] = useState(false)
 
   const handleSearch = async () => {
     /* double check that this is the correct axios call route */
@@ -44,25 +45,23 @@ function App() {
   //// AUTHENTICATION
 
   const logOut = () => {
-    setLogIn(false)
+    // setLogIn(false)
+    setAuthenticated(false)
     localStorage.clear()
-    history.push('/')
+    // history.push('/')
   }
 
   const getToken = () => {
-    // let token = localStorage.getItem('token')
-    // if (token) {
-    //   return setLogIn(true)
-    // }
+    let token = localStorage.getItem('token')
+    if (token) {
+      return setAuthenticated(true)
+    }
   }
 
   //// FUNCTIONS
 
   const toggleMenu = () => {
-    opened === true ?
-      setOpen(()=> (false))
-      :
-      setOpen(()=> (true))
+    opened === true ? setOpen(() => false) : setOpen(() => true)
   }
 
   //// ON LOAD
@@ -75,21 +74,23 @@ function App() {
 
   return (
     <div className="App">
-      <NavBar 
-        loggedIn={loggedIn} 
-        logOut={logOut} 
-        userID={userID}
-        openMenu={toggleMenu} 
-        handleChange={handleChangeSearch}
-        handleSearch={handleSearch}
-      />
-      <NavMobile 
+      <NavBar
+        loggedIn={loggedIn}
         logOut={logOut}
         userID={userID}
-        opened={opened} 
-        closeMenu={toggleMenu} 
+        openMenu={toggleMenu}
         handleChange={handleChangeSearch}
         handleSearch={handleSearch}
+        authenticated={authenticated}
+      />
+      <NavMobile
+        logOut={logOut}
+        userID={userID}
+        opened={opened}
+        closeMenu={toggleMenu}
+        handleChange={handleChangeSearch}
+        handleSearch={handleSearch}
+        authenticated={authenticated}
       />
       <Switch>
         <Route
@@ -101,15 +102,13 @@ function App() {
               setLogIn={setLogIn}
               history={history}
               setUserID={setUserID}
+              authenticated={authenticated}
             />
           )}
         />
         <Route
           path="/sign-up"
-          component={(props) => 
-            <SignupPage 
-              {...props} 
-            />}
+          component={(props) => <SignupPage {...props} />}
         />
         <Route
           path="/home/:user_id"
@@ -122,17 +121,15 @@ function App() {
               setUserID={setUserID}
               setUserLocation={setUserLocation}
               history={history}
+              authenticated={authenticated}
             />
           )}
         />
         <Route
           path="/user-profile/:user_id"
-          component={(props) => 
-            <ProfilePage 
-              {...props} 
-              loggedIn={loggedIn} 
-              userID={userID}
-            />}
+          component={(props) => (
+            <ProfilePage {...props} loggedIn={loggedIn} userID={userID} />
+          )}
         />
         <Route
           path="/new-pet/:user_id"
@@ -148,43 +145,33 @@ function App() {
         <Route
           path="/pet-profile/:pet_id"
           component={(props) => (
-            <PetPage 
-              {...props} 
-              loggedIn={loggedIn} 
-              userID={userID} 
+            <PetPage
+              {...props}
+              loggedIn={loggedIn}
+              userID={userID}
+              authenticated={authenticated}
             />
           )}
         />
         <Route
           path="/results"
           component={(props) => (
-            <SearchResultsPage 
-              {...props} 
-              loggedIn={loggedIn} 
+            <SearchResultsPage
+              {...props}
+              loggedIn={loggedIn}
+              authenticated={authenticated}
             />
           )}
         />
         <Route
           path="/browse"
-          component={(props) => 
-            <BrowsePage 
-              {...props} 
-            />}
+          component={(props) => <BrowsePage {...props} />}
         />
         <Route
           path="/browse-result/:specie"
-          component={(props) => 
-            <BrowseResultsPage 
-              {...props} 
-            />}
+          component={(props) => <BrowseResultsPage {...props} />}
         />
-        <Route 
-          path="/quiz" 
-          component={(props) => 
-            <PetQuizPage 
-              {...props} 
-            />} 
-        />
+        <Route path="/quiz" component={(props) => <PetQuizPage {...props} />} />
       </Switch>
     </div>
   )
