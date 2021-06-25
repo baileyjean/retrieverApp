14cd85547364b971e8772cf3e29394ebe75cdaee
Bailey Jean <baileyjleavitt@gmail.com>
2021-06-25 13:50:34 -0700

    Fix config.js

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'retrieverDB_production',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
